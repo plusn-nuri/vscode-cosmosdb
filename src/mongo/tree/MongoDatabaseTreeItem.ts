777fc062b34209c6d8f3a4267c843bfb28319eed
Stephen Weatherford <Stephen.Weatherford@microsoft.com>
2018-03-30 18:01:54 -0700

    Validate mongo path

--- src/mongo/tree/MongoDatabaseTreeItem.ts
+++ src/mongo/tree/MongoDatabaseTreeItem.ts
@@ -3,14 +3,23 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import * as cp from "child_process";
 import * as vscode from 'vscode';
+import * as os from "os";
 import * as path from 'path';
+import * as fse from 'fs-extra';
 import { MongoClient, Db, Collection } from 'mongodb';
 import { Shell } from '../shell';
 import { IAzureParentTreeItem, IAzureTreeItem, IAzureNode, UserCancelledError } from 'vscode-azureextensionui';
 import { DialogBoxResponses } from '../../constants';
 import { MongoCollectionTreeItem } from './MongoCollectionTreeItem';
 import { MongoCommand } from '../MongoCommand';
+import * as vscodeUtils from '../../utils/vscodeUtils';
+import { resolve } from "vscode-languageserver/lib/files";
+import { cpUtils } from "../../utils/cpUtils";
+
+const mongoShellPathSettingKey = 'mongo.shell.path';
+let output = vscodeUtils.getOutputChannel();
 
 export class MongoDatabaseTreeItem implements IAzureParentTreeItem {
 	public static contextValue: string = "mongoDb";
@@ -119,20 +128,107 @@ export class MongoDatabaseTreeItem implements IAzureParentTreeItem {
 	}
 
 	executeCommandInShell(command: MongoCommand): Thenable<string> {
+		output.appendLine(`Running command in Mongo shell: "${command.text}"...`);
+
 		return this.getShell().then(shell => shell.exec(command.text));
 	}
 
-	private getShell(): Promise<Shell> {
-		const shellPath = <string>vscode.workspace.getConfiguration().get('mongo.shell.path')
+	private async validateMongoShellPath(path: string): Promise<boolean> {
+		// We can't simply check if the file exists, because it may consist of arguments, or be a docker exec etc. ("docker exec -it mongodb mongo")
+
+		await cpUtils.executeCommand(output, undefined, )
+
+		let cmdOutput: string = '';
+		let cmdOutputIncludingStderr: string = '';
+
+		await new Promise((resolve: () => void, reject: (e: Error) => void): void => {
+			const options: cp.SpawnOptions = {
+				shell: true
+			};
+			const childProc: cp.ChildProcess = cp.spawn(path, undefined, options);
+
+			//output.appendLine('Running command: "{0} {1}"...', command, formattedArgs);
+			resolve()
+
+			// childProc.stdout.on('data', (data: string | Buffer) => {
+			// 	data = data.toString();
+			// 	cmdOutput = cmdOutput.concat(data);
+			// 	cmdOutputIncludingStderr = cmdOutputIncludingStderr.concat(data);
+			// 	if (outputChannel) {
+			// 		outputChannel.append(data);
+			// 	}
+			// });
+
+			// childProc.stderr.on('data', (data: string | Buffer) => {
+			// 	data = data.toString();
+			// 	cmdOutputIncludingStderr = cmdOutputIncludingStderr.concat(data);
+			// 	if (outputChannel) {
+			// 		outputChannel.append(data);
+			// 	}
+			// });
+
+			// childProc.on('error', reject);
+			// childProc.on('close', (code: number) => {
+			// 	if (code !== 0) {
+			// 		// We want to make sure the full error message is displayed to the user, not just the error code.
+			// 		// If outputChannel is defined, then we simply call 'outputChannel.show()' and throw a generic error telling the user to check the output window
+			// 		// If outputChannel is _not_ defined, then we include the command's output in the error itself and rely on AzureActionHandler to display it properly
+			// 		if (outputChannel) {
+			// 			outputChannel.show();
+			// 			reject(new Error(localize('azFunc.commandErrorWithOutput', 'Failed to run "{0}" command. Check output window for more details.', command)));
+			// 		} else {
+			// 			reject(new Error(localize('azFunc.commandError', 'Command "{0} {1}" failed with exit code "{2}":{3}{4}', command, formattedArgs, code, os.EOL, cmdOutputIncludingStderr)));
+			// 		}
+			// 	} else {
+			// 		if (outputChannel) {
+			// 			outputChannel.appendLine(localize('finishedRunningCommand', 'Finished running command: "{0} {1}".', command, formattedArgs));
+			// 		}
+			// 		resolve();
+			// 	}
+		});
+
+		return cmdOutput;
+	}
+
+	private async getShell(): Promise<Shell> {
+		let shellPath = <string>vscode.workspace.getConfiguration().get(mongoShellPathSettingKey)
 		if (!shellPath) {
-			return <Promise<null>>vscode.window.showInputBox({
-				placeHolder: "Configure the path to the mongo shell executable",
+			let userValue = await vscode.window.showInputBox({
+				prompt: `Specify the full path to the mongo shell executable (this value can be changed later via the VS Code setting '${mongoShellPathSettingKey}')`,
 				ignoreFocusOut: true
-			}).then(value => vscode.workspace.getConfiguration().update('mongo.shell.path', value, true)
-				.then(() => this.createShell(value)));
-		} else {
-			return this.createShell(shellPath);
+			});
+			try {
+				let stats: fse.Stats = await fse.stat(userValue);
+				const mongoExe = os.platform().startsWith('win') ? "mongo.exe" : "mongo";
+				if (stats.isDirectory()) {
+					// Automatically add the executable name to the folder
+					userValue = path.join(userValue, mongoExe);
+				}
+			}
+			catch (error) {
+				// ignore
+			}
+
+			let pathExists = false;
+			try {
+				pathExists = await fse.pathExists(userValue);
+			} catch (error) {
+				// ignore
+			}
+
+			if (!pathExists) {
+				if (await vscode.window.showWarningMessage(
+					`The location "${userValue}" does not appear to point to a valid executable file. Do you still want to use this location for the mongo shell executable?`,
+					DialogBoxResponses.Yes, DialogBoxResponses.Cancel) !== DialogBoxResponses.Yes) {
+					throw new UserCancelledError();
+				}
+			}
+
+			vscode.workspace.getConfiguration().update(mongoShellPathSettingKey, userValue, true);
+			shellPath = userValue;
 		}
+
+		return await this.createShell(shellPath);
 	}
 
 	private async createShell(shellPath: string): Promise<Shell> {
